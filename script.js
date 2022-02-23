ee4545459afb8d8d76a02fd29b0839a748aac64b
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2022-02-23 16:29:58 +0100

    rickroll easter egg hozzáadva!

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function openDiscordInvite() {
     window.open("https://discord.gg/abjyRPYDJz", "Discord Invite", "width=500, height=500, top=150, left=500");
 }
+
+function openRickroll() {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Rickroll", "width=500, height=500, top=150, left=500");
+}
+
+const rickRollParameters = new URLSearchParams(window.location.search);
+
+if (rickRollParameters.get("rickroll") === "true") {
+    var acceptRickrollPrompt = prompt(" NA JÓVAN MÁR HOGY SIKERÜLT EZT MEGTALÁLNOD MÁ \n Jó akkor ha eddig kutattál, akkor itt a videó... DE AKKOR ÍRD IDE HOGY 'IGENIS URAM!!'")
+    if (acceptRickrollPrompt == "IGENIS URAM!!") {
+        openRickroll()
+    }
+}
